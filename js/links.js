60ee8ea6aa183db3374670fbd77ae8e74238e8a5
thezaff <zkhodjaev@wiut.uz>
2017-11-15 01:40:48 +0500

    aa

--- js/links.js
+++ js/links.js
@@ -1,9 +1,8 @@
 var links = [
-    '<link rel="stylesheet" href="node_modules/bulma/css/bulma.css">',
+    '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.css">',
     '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">',
     '<link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">',
     '<link href="https://fonts.googleapis.com/css?family=Concert+One" rel="stylesheet">',
-    '<link rel="stylesheet" href="/node_modules/owl.carousel/dist/assets/owl.carousel.min.css">',
     /*  ADD CSS BETWEEN THESE */
 
     /* LINES */
